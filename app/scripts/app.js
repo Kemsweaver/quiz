42eb8eadb50441e1874c0407f5da0fae3f56d2bf
PROG-01 <prog-01@iMac-de-PROG-01.local>
2016-04-28 17:20:15 -0500

    blend effect on the video

---
+++ app/scripts/app.js
@@ -0,0 +1,92 @@
+var App = (function (window, $) {
+
+  'use strict';
+
+  var
+
+    elementType = Modernizr.touch ? '<img>' : '<video>',
+
+    elementSrc = Modernizr.touch ? 'images/cover.jpg' : 'media/teaser.mp4',
+
+    element = $(elementType, {}),
+
+    root = $('#root'),
+    
+    container = root.find('.o-artwork__container'),
+
+    width = $(window).width(),
+
+    height = $(window).height(),
+
+    init = function () {
+
+
+      createScene();
+    },
+
+    createScene = function () {
+      console.log(Modernizr.touch);
+      element.attr('src', elementSrc);
+
+      if ('autoplay' in element[0]) {
+        element.attr('autoplay', 'autoplay');
+        element.attr('loop', 'loop');
+      }
+
+
+      container.append(element);
+
+      // element.attr('src', elementSrc);
+      // element.css({ position:'absolute' });
+
+      setFullScreen();
+
+
+
+    },
+
+    setFullScreen = function () {
+      
+      var windowWidth = $(window).width(),
+        windowHeight = $(window).height(),
+        aspectRatio = Math.min(windowWidth / element.width(), windowHeight / element.height());
+
+      element.css({
+        'min-height': 0,
+        'min-width': 0,
+        'position': 'absolute',
+      });
+
+      element.parent().width(windowWidth).height(windowHeight);
+
+      var shift = 0;
+      if (windowWidth / windowHeight > aspectRatio) {
+        element.width(windowWidth).height('auto');
+        // shift the element up
+        var height = element.height();
+        shift = (height - windowHeight) / 2;
+        if (shift < 0) {
+          shift = 0;
+        }
+        element.css("top", -shift);
+      } else {
+        element.width('auto').height(windowHeight);
+        // shift the element left
+        var width = element.width();
+        shift = (width - windowWidth) / 2;
+        if (shift < 0) {
+          shift = 0;
+        }
+        element.css("left", -shift);
+      }
+
+    };
+
+
+
+
+  return {
+    init: init
+  }
+
+})(window, jQuery);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+(function (global) {
+  
+  'use strict';
+  
+  App.init();
+  
+})(window);
